5b70349112f373b6bc7fd1b056c0f777f636903d
Jacob Gonzalez <jacobgonzalez5252@hotmail.com>
2015-07-08 03:39:57 +0930

    removed a few boring programs

--- postReq.py
+++
@@ -1,7 +0,0 @@
-import requests as r
-name = raw_input('Name: ')
-req = r.post('http://localhost:8000', data=name)
-print(req.text)
-print('===================\n\n')
-req = r.get('http://localhost:8000')
-print req.text
--- server.py
+++
@@ -1,7 +0,0 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind(('0.0.0.0', 8001))
-s.serve_forever()
-
-s.recv(4)
